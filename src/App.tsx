673be3a025b8b7e0e74197e9550b8ca9aa7102a9
endorf8n <volodymyr.konopka@gmail.com>
2024-01-29 20:02:41 +0200

    start making routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Teachers from "../src/pages/Teachers";
+import Favorites from "../src/pages/Favorites";
+
 export const App = () => {
   return (
-    <div className="text-xl font-roboto">
-      First project using TypeScript and Tailwind CSS
+    <div>
+      <h1>Welcome to awesome app</h1>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/teachers">Teachers</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teachers" element={<Teachers />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
